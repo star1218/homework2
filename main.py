f0def2c354818b641a1fe6cc35e9844219208c4d
igork <igorkaaristocrat@gmail.com>
2024-03-01 16:01:37 +0200

    commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# list=[1,2,3,4,5]
+# list=[1,2,3,4,5] #1
 # def list_numbers(lst):
 #     result=1
 #     for num in lst:
@@ -6,7 +6,7 @@
 #     return result
 # print(list_numbers(list))
 
-# list=[1,2,3,4,5,6,7,8,9]
+# list=[1,2,3,4,5,6,7,8,9]#2
 # operation=("Минимум,Максимум")
 # def list_numbers(lst):
 #     max_value=max(lst)
@@ -16,7 +16,7 @@
 #     return max_value,min_value
 # print(list_numbers(list))
 
-# my_list =[1,2,3,4,5,6,7,8,9]
+# my_list =[1,2,3,4,5,6,7,8,9]#3
 #
 # def list_numbers(lst):
 #     even_numbers=[]
@@ -26,7 +26,7 @@
 #     return even_numbers
 # print(list_numbers(my_list))
 
-# my_list = [1, 2, 3, 4, 5]
+# my_list = [1, 2, 3, 4, 5]#4
 # number_to_remove=5
 #
 # def list_remove_numbers(lst,num):
